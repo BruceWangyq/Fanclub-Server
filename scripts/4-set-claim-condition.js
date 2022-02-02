89987b1617cd7375744dd4662261cb0e174c1f25
BruceWangyq <brucewangyq@gmail.com>
2022-02-02 15:27:24 -0500

    feat: added utils

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -1,32 +1,32 @@
 // import sdk from './1-initialize-sdk.js';
-const { sdk } = require('./1-initialize-sdk');
+const { sdk } = require("./1-initialize-sdk");
 
 const bundleDrop = sdk.getBundleDropModule(
-  '0xfd1e14bA0aA9A5ff464c466cb84e6eA94693fDcD'
+  "0xfd1e14bA0aA9A5ff464c466cb84e6eA94693fDcD"
 );
 
 exports.updateWhitelist = async () => {
-  console.log('haef');
+  console.log("haef");
   try {
     const factory = bundleDrop.getClaimConditionFactory();
     // const result = await bundleDrop.getAllClaimConditions(0);
 
     // Specify conditions.
-    const allowList = ['0xaf5144eCca7B93F58Fb4239d052c73e9063Ca3af'];
+    const allowList = ["0xaf5144eCca7B93F58Fb4239d052c73e9063Ca3af"];
     const claimPhase = factory.newClaimPhase({
       startTime: new Date(),
-      maxQuantity: 10,
+      maxQuantity: 10000,
       maxQuantityPerTransaction: 1,
     });
 
     claimPhase.setSnapshot(allowList);
 
-    await bundleDrop.setClaimCondition('0', factory);
+    await bundleDrop.setClaimCondition("0", factory);
     console.log(
-      '✅ Successfully set claim condition on bundle drop:',
+      "✅ Successfully set claim condition on bundle drop:",
       bundleDrop.address
     );
   } catch (error) {
-    console.error('Failed to set claim condition', error);
+    console.error("Failed to set claim condition", error);
   }
 };
